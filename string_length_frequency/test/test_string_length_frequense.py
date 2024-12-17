4723731a6611019c607f36969bb1e342711bd014
tuyendt <tuyendt@gmail.com>
2024-12-17 17:50:51 +0700

    fix test_find_most_frequent_string_lengths

--- string_length_frequency/test/test_string_length_frequense.py
+++ string_length_frequency/test/test_string_length_frequense.py
@@ -6,19 +6,22 @@ class TestStringLengthFrequency(unittest.TestCase):
         """
         This function is likely designed to find the most frequent string lengths in a given dataset.
         """
-
+        # normal case
         input_strings = ['a', 'ab', 'abc', 'cd', 'def', 'gh']
         result = find_most_frequent_string_lengths(input_strings)
         self.assertEqual(result, ['ab', 'cd', 'gh'])
-        
+
+        # case: different lengths but the same number of occurrences
         input_strings = ['a', 'bb', 'ccc']
         result = find_most_frequent_string_lengths(input_strings)
         self.assertEqual(result, ['a', 'bb', 'ccc'])
 
-        input_strings = ['abc', 'def', 'ghi']
+        # case: two different lengths with a frequency greater than one time
+        input_strings = ['aa', 'abc', 'bb', 'def', 'cc', 'ghi']
         result = find_most_frequent_string_lengths(input_strings)
-        self.assertEqual(result, ['abc', 'def', 'ghi'])
+        self.assertEqual(result, ['aa', 'abc', 'bb', 'def', 'cc', 'ghi'])
 
+        # case: empty array
         input_strings = []
         result = find_most_frequent_string_lengths(input_strings)
         self.assertEqual(result, [])
